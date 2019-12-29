8864c22713dfc679063eae52b0fdb67b5992ac3f
Advaith Sai <advaith.maddipatla@gmail.com>
2019-12-29 22:09:56 +0400

    minor change 804

--- Easy Problems/804. Unique Morse Code Words.py
+++ Easy Problems/804. Unique Morse Code Words.py
@@ -6,5 +6,4 @@ def uniqueMorseRepresentations(words):
         for ch in k:
             s = s + l[ord(ch) - 97]
         str.append(s)
-    return (len(set(str)))
-print(uniqueMorseRepresentations(["rwjje","aittjje","auyyn","lqtktn","lmjwn"]))
+    return len(set(str))
