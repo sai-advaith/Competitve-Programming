bd9cade3c8e43f3a4d4ec6911acec414dbdb54aa
Advaith Sai <advaith.maddipatla@gmail.com>
2019-12-30 12:09:07 +0400

    shit solution

---
+++ Easy Problems/344. Reverse String.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public static void reverseString(char[] s) {
+        char temp;
+        for (int i = 0; i<s.length/2;i++)
+        {
+            temp = s[i];
+            s[i] = s [s.length-i-1];
+            s[s.length-i-1] = temp;
+            
+        }
+        for (int i =0;i<s.length;i++)
+        {
+            System.out.print(s[i]+" ");
+        }
+    }
+}
