42c50da3017c58dab6aab8f5ccef340e82f1177b
Advaith Sai <advaith.maddipatla@gmail.com>
2020-02-13 10:22:28 +0000

    500

--- Leetcode/500. Keyboard Row.py
+++ Leetcode/500. Keyboard Row.py
@@ -1,30 +1,32 @@
-def findWords(words):
-	r1 = ['q','w','e','r','t','y','u','i','o','p']
-	r2 = ['a','s','d','f','g','h','j','k','l']
-	r3 = ['z','x','c','v','b','n','m']
-	k = 0
-	s = []
-	r = []
-	g = True
-	for i in words:
-		i = (i.lower())
-		if i[0] in r1:
-			k = 1
-		elif i[0] in r2:
-			k = 2
-		elif i[0] in r3:
-			k = 3
-		for ch in range(1,len(i)):
-			if k == 1:
-				g = g and (i[ch] in r1)
-			elif k == 2:
-				g = g and (ch in r2)
-			elif k == 3:
-				g= g and (ch in r3)
-			print(g)
-		s.append(g)
-	for i in range(len(s)):
-		if (s[i]):
-			r.append(words[i])
-	return r
-print(findWords(["Hello","Alaska","Dad","Peace"]))
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        r1 = ['q','w','e','r','t','y','u','i','o','p']
+        r2 = ['a','s','d','f','g','h','j','k','l']
+        r3 = ['z','x','c','v','b','n','m']
+        k = 0
+        s = []
+        def checkRow(s,k):
+            g = True
+            for ch in s:
+                if k == 1 and ch not in r1:
+                    g  = False
+                    break
+                elif k == 2 and ch not in r2:
+                    g = False
+                    break
+                elif k == 3 and ch not in r3:
+                    g = False
+                    break
+            return g
+        for i in words:
+            d = (i.lower())
+            if d[0] in r1:
+                k = 1
+            elif d[0] in r2:
+                k = 2
+            elif d[0] in r3:
+                k = 3
+            g = checkRow(d,k)
+            if (g):
+                s.append(i)
+        return s
