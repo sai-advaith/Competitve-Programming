ad0e953996e9896da590e923e0d68dcfe550b7d6
Advaith Sai <advaith.maddipatla@gmail.com>
2020-03-03 00:30:09 +0000

    changes

--- Leetcode/134. Gas Station.py
+++ Leetcode/134. Gas Station.py
@@ -1,15 +1,15 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        start, curr_sum, total_sum =0, 0, 0
+        start, current_gas, total_gas =0, 0, 0
         for index in range(len(gas)):
             diff = gas[index] - cost[index]
-            total_sum += diff
-            curr_sum += diff
+            total_gas += diff
+            current_gas += diff
             
-            if curr_sum < 0:
+            if current_gas < 0:
                 start = index + 1
-                curr_sum = 0
+                current_gas = 0
                 
-        if total_sum >= 0:
+        if total_gas >= 0:
             return start
         return -1
